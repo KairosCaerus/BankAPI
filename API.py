7c4595887841df7db32f04a9adf3f59a5c93dcc8
kairoscaerus <hkuriseti@gmail.com>
2020-11-19 15:55:53 -0800

    created script to initialize the database

--- API.py
+++ API.py
@@ -1,1 +1,16 @@
-#testing testing
+import flask
+import sqlite3
+
+
+app = flask.Flask(__name__)
+
+x = 5
+
+@app.route('/', methods=['GET'])
+def home():
+    dbconn = sqlite3.connect('testdb.db')
+    cursor = dbconn.cursor()
+    return "You are monkey"
+
+
+app.run()
---
+++ dbinit.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def clearDB(c):
+    c.execute('''SELECT NAME FROM sqlite_master WHERE type="table"''')
+    records = c.fetchall()
+    for row in records:
+        x = f'DROP TABLE {row[0]}'
+        c.execute(x)
+
+
+def initDB(c):
+    clearDB(c)
+    c.execute('''CREATE TABLE users (username text UNIQUE, password text, firstName text, lastName text)''')
+    c.execute('''CREATE TABLE accounts (accountID integer UNIQUE, owner text, pinNum integer, balance real)''')
+
+
+connection = sqlite3.connect('testdb.db')
+cursor = connection.cursor()
+initDB(cursor)
+connection.close()
