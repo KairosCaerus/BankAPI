5203140799bffebd8f67b1267d3794aeb02e7524
kairoscaerus <hkuriseti@gmail.com>
2020-11-22 19:40:43 -0800

    added request to get all registered users

--- API.py
+++ API.py
@@ -1,17 +1,20 @@
 import flask
 import sqlite3
 
-from flask import request
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
+MAIN_DB = 'testdb.db'
+
 
 @app.route('/', methods=['GET'])
 def home():
     return "You are monkey"
 
+
 @app.route('/users/add', methods=['POST'])
 def addUser():
-    conn = sqlite3.connect('testdb.db')
+    conn = sqlite3.connect(MAIN_DB)
     c = conn.cursor()
     newUser = (
         request.args["username"],
@@ -21,7 +24,7 @@ def addUser():
     )
 
     query = f'SELECT username FROM users WHERE username = ?'
-    c.execute(query, (request.args["username"], ))
+    c.execute(query, (request.args["username"],))
     if len(c.fetchall()) != 0:
         conn.close()
         return "That user already exists."
@@ -30,7 +33,33 @@ def addUser():
     conn.commit()
     conn.close()
 
-    return "Something happened"
+    return "Account successfully created"
 
+
+@app.route('/users/all', methods=['GET'])
+def getAllUsers():
+    conn = sqlite3.connect(MAIN_DB)
+    c = conn.cursor()
+
+    c.execute('SELECT * FROM users')
+    results = c.fetchall()
+
+    conn.close()
+
+    print(results)
+
+    return toJSON(results)
+
+def toJSON(target):
+    results = []
+    for row in target:
+        dictForRow = {
+            'username' : row[0],
+            'password' : row[1],
+            'firstname' : row[2],
+            'lastname' : row[3]
+        }
+        results.append(dictForRow)
+    return jsonify(results)
 
 app.run()
