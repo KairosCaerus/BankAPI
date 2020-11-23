55c17b713157b6adbd0129ab313371f41ba01bfa
kairoscaerus <hkuriseti@gmail.com>
2020-11-22 19:17:01 -0800

    finished add user, TODO: configure return statement

--- API.py
+++ API.py
@@ -1,16 +1,36 @@
 import flask
 import sqlite3
 
+from flask import request
 
 app = flask.Flask(__name__)
 
-x = 5
-
 @app.route('/', methods=['GET'])
 def home():
-    dbconn = sqlite3.connect('testdb.db')
-    cursor = dbconn.cursor()
     return "You are monkey"
+
+@app.route('/users/add', methods=['POST'])
+def addUser():
+    conn = sqlite3.connect('testdb.db')
+    c = conn.cursor()
+    newUser = (
+        request.args["username"],
+        request.args["password"],
+        request.args["firstname"],
+        request.args["lastname"]
+    )
+
+    query = f'SELECT username FROM users WHERE username = ?'
+    c.execute(query, (request.args["username"], ))
+    if len(c.fetchall()) != 0:
+        conn.close()
+        return "That user already exists."
+
+    c.execute('INSERT INTO users VALUES (?,?,?,?)', newUser)
+    conn.commit()
+    conn.close()
+
+    return "Something happened"
 
 
 app.run()
